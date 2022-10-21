9c3b683f3ad67eea1814c48bb2988d1e7937d214
antoine <antoine.guingand@wanadoo.fr>
2022-10-21 14:30:28 +0200

    Fetch API config

--- src/Jigsaw.php
+++ src/Jigsaw.php
@@ -3,6 +3,7 @@
 namespace Code16\Jigsaw;
 
 use Code16\Jigsaw\Listeners\FetchCollections;
+use Code16\Jigsaw\Listeners\FetchConfig;
 use Code16\Jigsaw\Listeners\UpdateCollections;
 use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Facade;
@@ -14,6 +15,7 @@ class Jigsaw
     public static function init(EventBus $events, Container $container): void
     {
         $events->beforeBuild(FetchCollections::class);
+        $events->beforeBuild(FetchConfig::class);
         $events->afterCollections(UpdateCollections::class);
     
         Facade::setFacadeApplication($container);
