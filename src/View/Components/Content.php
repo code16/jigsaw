574ddebf2c358ba9910a61d5ec02badd9f01af72
antoine <antoine.guingand@wanadoo.fr>
2022-10-28 18:16:16 +0200

    Handle heading-level

--- src/View/Components/Content.php
+++ src/View/Components/Content.php
@@ -10,25 +10,48 @@ class Content extends Component
     public function __construct(
         public ?int $imageThumbnailWidth = null,
         public ?int $imageThumbnailHeight = null,
+        public ?string $headingLevel = null,
     ) {
     }
     
     public function transform(string $content): string
     {
-        $content = $this->transformImages($content);
+        $this->transformImages($content);
+        $this->transformHeadings($content);
         
         return $content;
     }
     
-    public function transformImages(string $content): string
+    protected function transformImages(string &$content)
     {
-        return preg_replace_callback(
+        $content = preg_replace_callback(
             '/<p>\s*<img src="([^"]+)" alt="([^"]*)"[^>]*>\s*<\/p>/',
             function ($matches) {
                 return Blade::render('<x-content-image :src="$src" :alt="$alt" />', ['src' => $matches[1], 'alt' => $matches[2]]);
             },
             $content
         );
+    }
+    
+    protected function transformHeadings(&$content)
+    {
+        if ($this->headingLevel) {
+            $level = $this->getHeadingLevel();
+            $content = preg_replace_callback(
+                "#(</?h)(\d)#",
+                fn ($matches) => $matches[1] . ($level + $matches[2] - 1),
+                $content
+            );
+        }
+    }
+    
+    public function getHeadingLevel(): ?int
+    {
+        if(is_string($this->headingLevel)) {
+            return (int)str_replace('h', '', $this->headingLevel);
+        }
+        
+        return $this->headingLevel;
     }
     
     public function render()
